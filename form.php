2ad41a6465318e797d0d332de6790f76d445707b
Aleksandr <aleksandr_ermakov@outlook.com>
2024-03-19 09:44:31 +0300

    homework-js-0

---
+++ form.php
@@ -0,0 +1,5 @@
+<?php
+echo "это бэк"
+var_dump($_GET);
+// $login = $_GET
+?>
---
+++ js/app-hw-0.js
@@ -0,0 +1,37 @@
+// #region task0
+
+let result = 8;
+let multiplier;
+multiplier = 2.5;
+
+restul = (8 * multiplier + 674) / 3.3;
+
+console.log(`multiplier !== '2,5':`, multiplier !== "2,5");
+// true
+
+let cat = `Кот Василий`;
+console.log(`let cat =`, cat);
+
+// cat = cat + ` лежит на печи`;
+cat = `${cat} + лежит на печи`;
+cat = `${cat} + лежит на печи`;
+
+console.log(`let cat =`, cat);
+
+// #endregion task0
+
+// #region task1
+
+let a = 12;
+let b = 21;
+
+console.log(`At first: let a = `, a, `let b = `, b);
+
+let m = b;
+
+b = a;
+a = m;
+
+console.log(`At the end: let a = `, a, `let b = `, b);
+
+// #endregion task1
