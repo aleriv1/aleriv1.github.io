1fbb77afae459560f2f93dce18d23a8f84e73f9e
Aleksandr <aleksandr_ermakov@outlook.com>
2024-03-21 14:44:44 +0300

    task 2 is done (?), dirt deleting

--- js_lesson_2/task-js-l2-decision/app-hw-2.js
+++ js_lesson_2/task-js-l2-decision/app-hw-2.js
@@ -3,28 +3,12 @@
 // console.log(`test`);
 let chessBoard = [];
 
-// for (let i = 0; i < 4; i++) {
 for (let i = 0; i < 8; i++) {
   let iSubArr = [];
   //
   for (let iSub = 0; iSub < 8; iSub++) {
-    // iSubArr.push(null);
     iSubArr.push("");
-    // iSubArr.push(iSub + 1);
   }
-
-  // if ((i + 1) % 2 !== 0) {
-
-  // if ((i + 1) % 2 !== 0) {
-  // for (let iSub = 0; iSub < 8; iSub++) {
-  // iSubArr.push(iSub + 1);
-  // }
-  // } else {
-  // for (let iSub = 0; iSub < 8; iSub++) {
-  // iSubArr.push(iSub + 2);
-  // }
-  // }
-  //
 
   chessBoard.push(iSubArr);
 }
@@ -37,12 +21,12 @@ chessBoard[7][6] = "white king";
 console.log(chessBoard);
 
 // #endregion task 1
+
 console.log("//---");
+
 // #region task 2
 
-// let chessStr = `<div class='grid-cont'>`;
-// let chessStr = "";
-let chessStr = " ";
+let chessStr = "";
 
 for (let item of chessBoard) {
   // console.log(item);
@@ -51,19 +35,17 @@ for (let item of chessBoard) {
     // console.log(itemSub);
 
     chessStr += `<div class="grid-cont__item">${itemSub}</div>`;
-
-    // if ((item.indexOf(itemSub) + 1) % 2 != 0) {
-    //   chessStr += `<div class="grid-cont__item">${itemSub}</div>`;
-    // } else {
-    //   chessStr += `<div class="grid-cont__item" style="background:#00000077">${itemSub}</div>`;
-    // }
   }
 }
 
 // chessStr += "</div>";
-console.log("//---");
+
 console.log(chessStr);
 let gridCont = (document.querySelector(".grid-cont").innerHTML = chessStr);
 
 let gridItem;
 // #endregion task 2
+
+// #region task 3
+
+// #endregion task 3
