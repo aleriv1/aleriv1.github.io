7c5214a3bc42c7f1cfdbaf406f9448c3b3379bf4
Aleksandr <aleksandr_ermakov@outlook.com>
2024-04-18 20:32:28 +0300

    chat, 20240418-203235

--- chat/src/partials/chat.hbs.js
+++ chat/src/partials/chat.hbs.js
@@ -8,21 +8,26 @@ const messages = [
   { id: 5, id_user: 200, name: "NG", message: "Go" },
 ];
 
-const tplMessage = `<div class="col-start-1 col-end-8 p-3 rounded-lg">
+const tplMessage = `
 {{#each messages}}
+<diVjv class="col-start-1 col-end-8 p-3 rounded-lg">
 <div class="flex flex-row items-center">
-<div
-class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-{{ name }}
+    <div
+        class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+        {{ name }}A
+    </div>
+    <div class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+        <div>{{ message }}</div>
+    </div>
 </div>
-<div class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-<div>{{ message }}</div>
 </div>
-</div>
-</div>;
 {{/each}}`;
 
 let temaplate = Handlebars.compile(tplMessage);
-let htmlMenu = temaplate({ messages: messages });
-console.log(htmlMenu);
-Handlebars.registerPartial("chat", htmlMenu);
+let htmlChat = temaplate({ messages: messages });
+console.log(htmlChat);
+Handlebars.registerPartial("chat", htmlChat);
+
+let root = document.querySelector(`div[data-id="chat"]`);
+
+root.innerHTML = htmlChat;
