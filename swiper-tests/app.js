5a32c821a6bc5eba40abefe64688db529c59a949
Aleksandr <aleksandr_ermakov@outlook.com>
2024-09-11 23:04:42 +0300

    swiper test grid and button

--- swiper-tests/app.js
+++ swiper-tests/app.js
@@ -6,7 +6,7 @@ let init = false;
 
 /* Which media query */
 function swiperMode() {
-  let mobile = window.matchMedia("(min-width: 0px) and (max-width: 768px)");
+  let mobile = window.matchMedia("(min-width: 0px) and (max-width: 767px)");
 
   // Enable (for mobile)
   if (mobile.matches) {
@@ -25,7 +25,8 @@ function swiperMode() {
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
-          hide: true,
+          type: "bullets",
+          // hide: true,
         },
 
         // Navigation arrows
@@ -58,4 +59,24 @@ window.addEventListener("load", function () {
 /* On Resize */
 window.addEventListener("resize", function () {
   swiperMode();
+});
+
+let expand = document.querySelector(".expand--brands");
+
+let mySwWr = document.querySelector(".my-swiper__wrapper");
+
+let buttonShow = true;
+
+console.log(`expand is: ${expand} | mySwWr is: ${mySwWr}`);
+
+expand.addEventListener("click", () => {
+  if (buttonShow) {
+    buttonShow = false;
+    mySwWr.style.height = "auto";
+    expand.innerHTML = "Скрыть";
+  } else {
+    buttonShow = true;
+    mySwWr.style.height = "184px";
+    expand.innerHTML = "Показать всё";
+  }
 });
