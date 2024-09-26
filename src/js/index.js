9c89e0f99f169bc4596db6ca8feaa83c5974c4ea
Aleksandr <aleksandr_ermakov@outlook.com>
2024-09-26 10:46:44 +0300

    the third swiper commercial offer

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
-// import '../scss/swiper/'
+// console.log('Works!')
+
 import '../scss/style.scss'
 
 import './swiper-bundle.min.js'
-console.log('Works!')
 
 import './swiper-with-my-addition.js'
 
