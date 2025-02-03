8d4c4fc33a32ecf538ea8e8236a1bf8418f5fe39
Aleksandr <aleksandr_ermakov@outlook.com>
2025-02-03 04:29:06 +0300

    todo-kata-react-3-new

---
+++ todo-kata-app-3-new/src/components/main/main.jsx
@@ -0,0 +1,21 @@
+import "./main.css"
+
+import Footer from "../footer"
+import TaskList from "../task-list"
+
+const MainSection = ({ todos, onDeleted, onToggleDone, onFilterChange, taskLeft, onDeleteAllDone, filter }) => {
+  return (
+    <section className="main">
+      <TaskList todos={todos}
+        onDeleted={onDeleted} 
+        onToggleDone={onToggleDone}/>
+      <Footer 
+      onFilterChange={onFilterChange} 
+      taskLeft = {taskLeft}
+      onDeleteAllDone={onDeleteAllDone}
+      filter={filter}/>
+    </section>
+  )
+}
+
+export default MainSection
