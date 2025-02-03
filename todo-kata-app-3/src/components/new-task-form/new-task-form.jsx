55ebe3ae1fe6e0da62661f7e78e66ce8e6f8dfd5
Aleksandr <aleksandr_ermakov@outlook.com>
2025-02-03 04:28:24 +0300

    tod-kata-app-3 rm

--- todo-kata-app-3/src/components/new-task-form/new-task-form.jsx
+++
@@ -1,9 +0,0 @@
-import './new-task-form.css'
-
-const NewTaskForm = () => {
-  return (
-    <input type="text" className="new-todo" placeholder="What needs to be done?" autoFocus />
-  )
-}
-
-export default NewTaskForm
