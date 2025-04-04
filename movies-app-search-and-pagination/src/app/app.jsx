88c523f98e918574c17f242772207b39db83acce
Aleksandr <aleksandr_ermakov@outlook.com>
2025-04-05 02:26:21 +0300

    movie-loading-search-and-pagination-in-process-question-input-pagination-return-combine-page

--- movies-app-search-and-pagination/src/app/app.jsx
+++ movies-app-search-and-pagination/src/app/app.jsx
@@ -8,9 +8,12 @@ import MovieList from '../movie-list'
 
 import './app.scss'
 
+const MOVIES_PER_PAGE = 6
+
 export default class App extends Component {
   state = {
     movies: [],
+    visibleMovies: [],
     loading: true,
     error: null,
     searchQuery: '',
@@ -20,16 +23,16 @@ export default class App extends Component {
 
   _fetchMovies = (query = 'return', page = 1) => {
     const realQuery = query.trim() || 'return'
-    // const apiPage = Math.floor(((page - 1) * 6) / 20) + 1
+    const apiPage = Math.floor(((page - 1) * MOVIES_PER_PAGE) / 20) + 1
     this.setState({ loading: true, error: null })
     // fetchMoviesByQuery(query, page)
-    fetchMoviesByQuery(realQuery, page)
+    // fetchMoviesByQuery(realQuery, page)
+    fetchMoviesByQuery(realQuery, apiPage)
       .then((data) => {
-        // const moviesPerPage = 6
+        const startIndex = ((page - 1) * MOVIES_PER_PAGE) % 20
+        const visibleMovies = data.results.slice(startIndex, startIndex + MOVIES_PER_PAGE)
 
-        this.setState({ movies: data.results, loading: false, totalResults: data.total_results })
-
-        // this.setState({ movies: data.results, loading: false, totalResults: data.total_results })
+        this.setState({ movies: data.results, visibleMovies, loading: false, totalResults: data.total_results })
       })
       .catch(() => {
         this.setState({ loading: false, error: 'Something went wrong, \nbut we do everything \nto RETURN \nyou joy' })
@@ -65,7 +68,8 @@ export default class App extends Component {
   }
 
   render() {
-    const { movies, loading, error, searchQuery, currentPage, totalResults } = this.state
+    // const { movies, loading, error, searchQuery, currentPage, totalResults } = this.state
+    const { visibleMovies, loading, error, searchQuery, currentPage, totalResults } = this.state
 
     return (
       <div className="app">
@@ -87,17 +91,18 @@ export default class App extends Component {
             style={{ margin: '20px auto', width: '90%', maxWidth: '600px', display: 'block' }}
           />
 
-          {loading && movies.length === 0 ? (
+          {/* {loading && movies.length === 0 ? ( */}
+          {loading && visibleMovies.length === 0 ? (
             <div className="loading-container-common">
               <Spin fullscreen />
             </div>
           ) : error ? (
             <Alert message={error} />
-          ) : movies.length === 0 ? (
+          ) : visibleMovies.length === 0 ? (
             <Alert message="No movies found" />
           ) : (
             <>
-              <MovieList movies={movies} loading={loading} />
+              <MovieList movies={visibleMovies} loading={loading} />
               <Pagination
                 current={currentPage}
                 total={totalResults}
