60466631c2e92917dc7f7a6d058f20213f5bd268
Aleksandr <aleksandr_ermakov@outlook.com>
2025-04-24 22:59:27 +0300

    it is crazy

--- movies-app-raiting-and-genres/src/app/app.jsx
+++ movies-app-raiting-and-genres/src/app/app.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-// import { Offline, Online } from 'react-detect-offline'
+import { Offline, Online } from 'react-detect-offline'
 import { Alert, Input, Pagination, Spin, Tabs } from 'antd'
 import debounce from 'lodash/debounce'
 
@@ -214,101 +214,100 @@ export default class App extends Component {
     return (
       <GenreProvider value={genres}>
         <div className="app">
-          {/* <Offline>
+          <Offline>
             <Alert
               message="You are offline"
               type="warning"
               description="Make sure you have an active internet connection"
               banner={true}
             />
-          </Offline> */}
+          </Offline>
 
-          {/* <Online> */}
-          <Tabs
-            activeKey={activeTab}
-            onChange={this.onTabChange}
-            items={[
-              {
-                key: 'search',
-                label: 'Search',
-                children: (
-                  <>
-                    <Input
-                      className="input-class"
-                      placeholder="Type to search..."
-                      value={searchQuery}
-                      onChange={this.handleInputSearch}
-                      disabled={loading}
-                      style={{ margin: '20px auto', width: '90%', maxWidth: '600px', display: 'block' }}
-                    />
-                    {loading && visibleMovies.length === 0 ? (
-                      <div className="loading-container-common">
-                        <Spin fullscreen />
-                      </div>
-                    ) : error ? (
-                      <Alert message={error} />
-                    ) : visibleMovies.length === 0 ? (
-                      <Alert message="No movies found" />
-                    ) : (
-                      <>
-                        <MovieList
-                          movies={visibleMovies}
-                          ratedMovies={ratedMovies}
-                          loading={loading}
-                          guestSessionId={this.state.guestSessionId}
-                        />
-                        <Pagination
-                          current={currentPage}
-                          total={totalResults}
-                          pageSize={MOVIES_PER_PAGE}
-                          onChange={this.onPageChange}
-                          style={{ margin: '20px auto', textAlign: 'center' }}
-                          disabled={loading}
-                          showSizeChanger={false}
-                        />
-                      </>
-                    )}
-                  </>
-                ),
-              },
-              {
-                key: 'rated',
-                label: 'Rated',
-                children: (
-                  <>
-                    {loading && ratedMovies.length === 0 ? (
-                      <div className="loading-container-common">
-                        <Spin fullscreen />
-                      </div>
-                    ) : error ? (
-                      <Alert message={error} />
-                    ) : ratedMovies.length === 0 ? (
-                      <Alert message="No rated movies found" />
-                    ) : (
-                      <>
-                        <MovieList
-                          // movies={ratedMovies}
-                          movies={visibleRatedMovies}
-                          loading={loading}
-                          guestSessionId={this.state.guestSessionId}
-                        />
-                        <Pagination
-                          current={ratedCurrentPage}
-                          total={ratedTotalResults}
-                          pageSize={MOVIES_PER_PAGE}
-                          onChange={this.onPageChange}
-                          style={{ margin: '20px auto', textAlign: 'center' }}
-                          disabled={loading}
-                          showSizeChanger={false}
-                        />
-                      </>
-                    )}
-                  </>
-                ),
-              },
-            ]}
-          />
-          {/* </Online> */}
+          <Online>
+            <Tabs
+              activeKey={activeTab}
+              onChange={this.onTabChange}
+              items={[
+                {
+                  key: 'search',
+                  label: 'Search',
+                  children: (
+                    <>
+                      <Input
+                        className="input-class"
+                        placeholder="Type to search..."
+                        value={searchQuery}
+                        onChange={this.handleInputSearch}
+                        disabled={loading}
+                        style={{ margin: '20px auto', width: '90%', maxWidth: '600px', display: 'block' }}
+                      />
+                      {loading && visibleMovies.length === 0 ? (
+                        <div className="loading-container-common">
+                          <Spin fullscreen />
+                        </div>
+                      ) : error ? (
+                        <Alert message={error} />
+                      ) : visibleMovies.length === 0 ? (
+                        <Alert message="No movies found" />
+                      ) : (
+                        <>
+                          <MovieList
+                            movies={visibleMovies}
+                            ratedMovies={ratedMovies}
+                            loading={loading}
+                            guestSessionId={this.state.guestSessionId}
+                          />
+                          <Pagination
+                            current={currentPage}
+                            total={totalResults}
+                            pageSize={MOVIES_PER_PAGE}
+                            onChange={this.onPageChange}
+                            style={{ margin: '20px auto', textAlign: 'center' }}
+                            disabled={loading}
+                            showSizeChanger={false}
+                          />
+                        </>
+                      )}
+                    </>
+                  ),
+                },
+                {
+                  key: 'rated',
+                  label: 'Rated',
+                  children: (
+                    <>
+                      {loading && ratedMovies.length === 0 ? (
+                        <div className="loading-container-common">
+                          <Spin fullscreen />
+                        </div>
+                      ) : error ? (
+                        <Alert message={error} />
+                      ) : ratedMovies.length === 0 ? (
+                        <Alert message="No rated movies found" />
+                      ) : (
+                        <>
+                          <MovieList
+                            movies={visibleRatedMovies}
+                            loading={loading}
+                            guestSessionId={this.state.guestSessionId}
+                          />
+                          <Pagination
+                            current={ratedCurrentPage}
+                            total={ratedTotalResults}
+                            pageSize={MOVIES_PER_PAGE}
+                            onChange={this.onPageChange}
+                            style={{ margin: '20px auto', textAlign: 'center' }}
+                            disabled={loading}
+                            showSizeChanger={false}
+                          />
+                        </>
+                      )}
+                    </>
+                  ),
+                },
+              ]}
+            />
+          </Online>
         </div>
       </GenreProvider>
     )
