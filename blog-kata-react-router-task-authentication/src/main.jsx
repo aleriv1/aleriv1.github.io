f90d4c16a75d050fe0524d9bac11a791274f4896
Aleksandr <aleksandr_ermakov@outlook.com>
2025-05-17 16:36:31 +0300

    blog authentication 0

---
+++ blog-kata-react-router-task-authentication/src/main.jsx
@@ -0,0 +1,8 @@
+// import { StrictMode } from 'react'
+// <StrictMode>
+// </StrictMode>
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(<App />)
