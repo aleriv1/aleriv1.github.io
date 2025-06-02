13bba3e2e578236798822d176f8d5b7dcc97a311
Aleksandr <aleksandr_ermakov@outlook.com>
2025-06-02 23:20:47 +0300

    cleaning

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 https://blog-router-react-kata-eai-task.vercel.app/
-
-Тут как-то сервер не работал; какое-то время использовал моки mockData.js, лежат в src
--- blog-kata-react-router-task-authentication/src/main.jsx
+++
@@ -1,8 +0,0 @@
-// import { StrictMode } from 'react'
-// <StrictMode>
-// </StrictMode>
-import { createRoot } from 'react-dom/client'
-
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(<App />)
